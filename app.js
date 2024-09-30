0c003920667373bc8e5887a47c22bcfed2c15761
smartThinkFaruk <smartthink.omarfaruk@gmail.com>
2024-09-30 12:30:59 +0600

    conditional rendaring done v-if v-else v-else-if

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
     data: {
         title: "Rafee",
-        age: 26,
+        // age: 26,
         isItTrue: true,
         cars: ['Ford', 'Toyota'],
         robot: {
@@ -13,11 +13,17 @@ new Vue({
         link: "https://www.google.com",
         myText: "Hello World",
         myHtml: "<strong>Hello World</strong>",
+        name: "Md Omar Faruk",
+        age: 15,
+        allowAge: 18,
     },
     methods: {
         greeting(){
             return this.title
         },
+        changeUser(){
+            this.name = "Rafee" ? true : false
+        }
 
     }
 })
